e327148b47b75719a6a3725bf5af8a7ee624314b
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2012-08-22 13:00:38 +0900

    update config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,3 +77,5 @@ WIFI_DRIVER_MODULE_NAME     :=  "bcm4329"
 WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/vendor/firmware/nvram_net.txt"
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/crespo/recovery/recovery_ui.c
